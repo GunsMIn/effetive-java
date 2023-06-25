7ab364f5f6fc51b8e84dc93a679ef916a232fe92
KimGunWoo9595 <medneter@naver.com>
2023-06-25 10:42:24 +0900

    Initial commit

---
+++ src/main/java/com/example/effetivejava/EffetiveJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.effetivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffetiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffetiveJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/effetivejava/EffetiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.effetivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffetiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
